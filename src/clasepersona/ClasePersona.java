64764da53594123e9a453b03eb8b75f3b7de5dfa
KarinaDani <rikes.kar@gmail.com>
2023-05-05 20:32:27 +0200

    Ejercicio 3 JuezLTI

--- src/clasepersona/ClasePersona.java
+++ src/clasepersona/ClasePersona.java
@@ -26,6 +26,7 @@ public class ClasePersona {
                 
                 
                 //error
+                //error
                 System.out.println("Procesado: " + persona.getNombre() + " " + persona.getApellidos() + "  nacido el " + persona.getFechaNacimiento());
             } catch (IllegalArgumentException ex) {
                 System.out.println("ERROR. Procesando siguiente persona");
